a243b76a4d19a6df2bc4bb4c1394232cc40fafdb
AlexanderTheGreatSK <alex.okrucky@gmail.com>
2022-02-16 19:42:19 +0100

    build

--- src/main/java/com/example/ija/HelloApplication.java
+++ src/main/java/com/example/ija/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.ija;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +13,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Canvas canvas = new Canvas(1000,1000);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+        graphicsContext.setLineWidth(2.0);
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.strokeRoundRect(10, 10, 50, 50, 10, 10);
+        graphicsContext.fillRoundRect(100, 10, 50, 50, 10, 10);
     }
 
     public static void main(String[] args) {
